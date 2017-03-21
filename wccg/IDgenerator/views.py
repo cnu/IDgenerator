2140047a226d7fb8f33b54b4c2b42cba528cb479
naren <makernaren@gmail.com>
2017-03-21 20:17:35 +0530

    Add basic views

--- wccg/IDgenerator/views.py
+++ wccg/IDgenerator/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def submit_idcard(request):
+    if request.method == 'POST':
+        print('inside ID card')
+        data = json.loads(request.body.decode('UTF-8'))
+        print data
+        return HttpResponse('ok')
+
+
+@csrf_exempt
+def ping(request):
+    print('inside ID card')
+    if request.method == 'GET':
+        return HttpResponse('pong')
