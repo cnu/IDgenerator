209f2d9c088fe96a92517053fe777810f83e5080
naren <makernaren@gmail.com>
2017-03-22 22:18:40 +0530

    Add first version of DB models

--- wccg/IDgenerator/models.py
+++ wccg/IDgenerator/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class RiderDetails(models.Model):
+    id = models.AutoField(primary_key=True)
+    rider_id = models.CharField(max_length=50, unique=True, db_index=True)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50)
+    dob = models.DateTimeField()
+    sex = models.CharField(max_length=6)
+    contact_number = models.CharField(max_length=15, db_index=True)
+    contact_email = models.CharField(max_length=50)
+    blood_group = models.CharField(max_length=20)
+    emergency_name = models.CharField(max_length=50)
+    emergency_number = models.CharField(max_length=15)
+    area = models.CharField(max_length=50)
+    address = models.TextField(null=True)
+    max_distance = models.IntegerField()
+    # chapter = JSONField()
+    # bicycle = JSONField()
+    # AOI = JSONField(default=None)
+    # terrain = JSONField(default=None)
+    goals = models.TextField()
+    expectations = models.TextField()
+    event_types = models.TextField()
+    comments = models.TextField()
+
+    class Meta:
+        db_table = 'rider_details'
