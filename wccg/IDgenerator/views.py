f1276aed21f3711e73125e7526b957c07c634af8
naren <makernaren@gmail.com>
2017-03-22 22:21:58 +0530

    Make prints compatible with python3

--- wccg/IDgenerator/views.py
+++ wccg/IDgenerator/views.py
@@ -6,9 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def submit_idcard(request):
     if request.method == 'POST':
-        print('inside ID card')
         data = json.loads(request.body.decode('UTF-8'))
-        print data
         validator(data)
         return HttpResponse('ok')
 
@@ -19,6 +17,5 @@ def validator(data):
 
 @csrf_exempt
 def ping(request):
-    print('inside ID card')
     if request.method == 'GET':
         return HttpResponse('pong')
