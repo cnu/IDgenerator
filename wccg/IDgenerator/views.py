fc0d997504240c2ea9f99ff0e5ff1dfb2846d4fd
naren <makernaren@gmail.com>
2017-03-23 21:57:29 +0530

    Do a dumb insert into DB

--- wccg/IDgenerator/views.py
+++ wccg/IDgenerator/views.py
@@ -1,6 +1,9 @@
 import json
+import traceback
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from IDgenerator.models import RiderDetails
 
 
 @csrf_exempt
@@ -8,11 +11,25 @@ def submit_idcard(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('UTF-8'))
         validator(data)
-        return HttpResponse('ok')
+        insert_successful = insert_db(data)
+        if insert_successful:
+            return HttpResponse('Inserted into database successfully')
+        else:
+            return HttpResponse('Insert failed')
 
 
 def validator(data):
     pass
+
+
+def insert_db(data):
+    db = RiderDetails(**data)
+    try:
+        db.save()
+        return True
+    except:
+        print(traceback.format_exc())
+        return False
 
 
 @csrf_exempt
