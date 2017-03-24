57e890352845cab0873a2af2b814ad0dfb800d19
naren <makernaren@gmail.com>
2017-03-24 22:37:15 +0530

    Add generate_rider_id() function
    It gets the last inserted rider_id and increases it by 1

--- wccg/IDgenerator/views.py
+++ wccg/IDgenerator/views.py
@@ -22,6 +22,23 @@ def validator(data):
     pass
 
 
+def generate_rider_id():
+    prefix = str(date.today().year)[2:]
+    try:
+        latest_id = Rider.objects.values('rider_id').latest('created_date')['rider_id']
+    except:
+        rider_id = prefix + 'CC' + '00001'
+        return rider_id
+    print(latest_id)
+    to_incr = latest_id.split('CC')[-1]
+    zeros_count = str(to_incr).count('0')
+    print('zeros_count', zeros_count)
+    next_id = str(int(to_incr) + 1).zfill(zeros_count+1)
+    print(prefix, next_id)
+    rider_id = prefix + 'CC' + next_id
+    return rider_id
+
+
 def insert_db(data):
     db = Rider(**data)
     try:
